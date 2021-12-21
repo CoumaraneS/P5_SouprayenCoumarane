a0179d703313b2ede90a1cdaa99173061c3f42f5
Coumarane <goomeshwar@yahoo.co.in>
2021-12-21 16:33:03 +0100

    Récupération de l'item

--- front/js/product.js
+++ front/js/product.js
@@ -9,4 +9,28 @@ let itemPrice = document.getElementById("price");
 let itemDescription = document.getElementById("description");
 let itemColors = document.getElementById("colors");
 
+//Récupération de l'item
+getItem();
+
+async function getItem() {
+    await fetch("http://localhost:3000/api/products/" + itemId)
+    .then((response) => response.json())
+    .then(item => {
+        img.setAttribute("src", item.imageUrl);
+        img.setAttribute("alt", item.altTxt);
+        itemTitle.textContent = item.name;
+        itemPrice.textContent = item.price;
+        itemDescription.textContent = item.description;
+        document.title = item.name;
+
+        for (let i=0; i < item.colors.length; i++) {
+            let color = document.createElement("option");
+            color.setAttribute("value", item.colors [i]);
+            color.textContent = item.colors [i];
+            itemColors.appendChild(color);
+        }
+    })
+}
+
+
 console.log();
