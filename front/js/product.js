568aac924789df3e921108b2158451453a24bed4
Coumarane <goomeshwar@yahoo.co.in>
2021-12-30 16:05:10 +0100

    cartItem modifiés

--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,15 @@
 const itemId = new URL(window.location.href).searchParams.get("id");
 
+console.log(itemId);
 //Récupération des éléments par ID et par classe
-let itemImg = document.querySelector(".item__img");
-let img = document.createElement("img");
-itemImg.appendChild(img);
+
+let itemImg = document.createElement("img");
+document.querySelector(".item__img").appendChild(itemImg);
 let itemTitle = document.getElementById("title");
 let itemPrice = document.getElementById("price");
 let itemDescription = document.getElementById("description");
 let itemColors = document.getElementById("colors");
+
 
 //Récupération de l'item
 getProduct();
@@ -16,54 +18,85 @@ async function getProduct() {
     await fetch("http://localhost:3000/api/products/" + itemId)
     .then((response) => response.json())
     .then(item => {
-        img.setAttribute("src", item.imageUrl);
-        img.setAttribute("alt", item.altTxt);
+        itemImg.setAttribute("src", item.imageUrl);  
+        //img.src=item.imageUrl;
+
+        itemImg.setAttribute("alt", item.altTxt);
+        //img.alt=item.altTxt;
+
         itemTitle.textContent = item.name;
         itemPrice.textContent = item.price;
         itemDescription.textContent = item.description;
         document.title = item.name;
 
-        for (let i=0; i < item.colors.length; i++) {
-            let color = document.createElement("option");
-            color.setAttribute("value", item.colors [i]);
-            color.textContent = item.colors [i];
-            itemColors.appendChild(color);
-        }
+        console.log(item.price); 
+
+        addToCartButton.addEventListener ("click", () => {
+            addToCart(item);
+        });
+
+        for(let cur of item.colors)
+           {
+               let color = document.createElement("option");
+               color.value=cur;
+               color.textContent=cur;
+               itemColors.appendChild(color);
+            
+            }
+
     });
 }
 
 //Ajout du ou des articles au panier
 let addToCartButton = document.getElementById("addToCart");
-addToCartButton.addEventListener ("click", () => {
-    addToCart();
-});
 
-function addToCart() {
+
+function addToCart(elementCur) {
+ 
+    let productId = itemId;
+    let productColor = document.querySelector("#colors").value;
+    let productQty = document.querySelector("#quantity").value;
+
+    
+//condition to verify if the quantity is 0 or less than 0 an alert if, zero or below zero
+
+    let cartItem = {
+        productId : itemId,
+        productColor : productColor,
+        productQty  : productQty,
+        productImg : elementCur.imageUrl,
+        productImgAlt : elementCur.altTxt,
+        itemTitle : elementCur.name,
+        productPrice : elementCur.price,
+
+        };
+
+
+    console.table(cartItem);
 
     if (localStorage.getItem("cart")) {
 
-        let cart = localStorage.getItem("cart");
-        console.log(cart);
+        let cart = JSON.parse(localStorage.getItem("cart"));
+
+        if (cart.filter(el=>{return (el.productId===cartItem.productId)&&(el.productColor===cartItem.productColor)}).length>0)
+        {cart.filter(el=>{return (el.productId===cartItem.productId)&&(el.productColor===cartItem.productColor)})[0].productQty = 
+        parseInt(cart.filter(el=>{return (el.productId===cartItem.productId)&&(el.productColor===cartItem.productColor)})[0].productQty) +parseInt(cartItem.productQty);
+        console.table(cart.filter(el=>{return (el.productId===cartItem.productId)&&(el.productColor===cartItem.productColor)})[0])
+    } else  
+        cart.push(cartItem);
+        
+        let itemCart = JSON.stringify(cart);
+        localStorage.setItem("cart", itemCart);
         
     } else {
-        
-        let productId = itemId;
-        let productColor = document.querySelector("#colors").value;
-        let productQty = document.querySelector("#quantity").value;
-
-        console.log(productId, productColor, productQty);
-
-        let cartItem = {
-            productId : itemId,
-            productColor : productColor,
-            productQty  : productQty
-        };
-
-        console.log(cartItem);
-
-        let itemCart = JSON.stringify(cartItem);
+      
+        let itemCart = JSON.stringify([cartItem]);
         localStorage.setItem("cart", itemCart);
 
-        alert("Article ajouté au panier");
     }
+  
+    alert("Article ajouté au panier"+ "\n" + "Quantité : " + productQty+ "\n" + "Couleur : " +  productColor);
+
 }
+
+
